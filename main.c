7bc17956d6c7400b9634f708db7d143fe4a7131f
Jean Pierre Dudey <me@jeandudey.tech>
2020-11-09 22:27:34 +0100

    Update Air Quality code
    Signed-off-by: Jean Pierre Dudey <me@jeandudey.tech>

--- main.c
+++ main.c
@@ -1,19 +1,17 @@
-/*   Copyright 2020 Locha Inc
+/*
+ * Copyright (C) 2020 Locha Inc
  *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
  */
 
 #include <string.h>
+
+#ifdef CPU_ESP32
+#include "adc_arch.h"
+#include "gp2y10xx_params.h"
+#endif
 
 #include "lvgl/lvgl.h"
 #include "lvgl_riot.h"
@@ -29,7 +27,9 @@
 #include "saul_reg.h"
 #include "xtimer.h"
 
-#define REFR_TIME          (5000)
+#include "thread.h"
+
+#define REFR_TIME          (600)
 
 static void airquality_create_humidity(lv_obj_t *parent);
 static void airquality_update_humidity(void);
@@ -57,11 +57,18 @@ static lv_obj_t *temperature_gauge;
 static lv_obj_t *temperature_label;
 static saul_reg_t *temperature_sensor;
 
+static lv_obj_t *pressure_gauge;
 static lv_obj_t *pressure_label;
 static saul_reg_t *pressure_sensor;
 
+static lv_obj_t *particulate_gauge;
 static lv_obj_t *particulate_label;
 static saul_reg_t *particulate_sensor;
+static phydat_t particulate;
+
+static lv_style_t style_box;
+
+#define PA_PER_MBAR (100U)
 
 /* C doesn't have a sane pow function that uses only integers, and us, the poor
  * men have to write our own, less tested alternative :-).
@@ -93,18 +100,16 @@ static void airquality_create_humidity(lv_obj_t *parent)
     }
     LOG_INFO("Humidity sensor found\n");
 
-    lv_coord_t grid_w_meter = lv_page_get_width_grid(parent, 3, 1);
-    lv_coord_t meter_h = lv_page_get_height_fit(parent);
-    lv_coord_t meter_size = LV_MATH_MIN(grid_w_meter, meter_h);
-
     humidity_lmeter = lv_linemeter_create(parent, NULL);
-    lv_obj_set_size(humidity_lmeter, meter_size, meter_size);
+    lv_obj_set_size(humidity_lmeter, 80, 80);
+    lv_obj_add_style(humidity_lmeter, LV_LINEMETER_PART_MAIN, &style_box);
     lv_linemeter_set_range(humidity_lmeter, 0, 100);
     lv_linemeter_set_value(humidity_lmeter, 0);
-    lv_linemeter_set_scale(humidity_lmeter, 240, 10);
+    lv_linemeter_set_scale(humidity_lmeter, 240, 11);
 
     humidity_label = lv_label_create(humidity_lmeter, NULL);
     lv_label_set_text(humidity_label, "Humidity");
+    lv_label_set_recolor(humidity_label, true);
     lv_obj_align(humidity_label, humidity_lmeter, LV_ALIGN_IN_BOTTOM_MID, 0, -10);
 }
 
@@ -137,7 +142,7 @@ static void airquality_update_humidity(void)
     size_t len = fmt_s16_dfp(str_hum, humidity.val[0], humidity.scale);
     str_hum[len] = '\0';
 
-    lv_label_set_text_fmt(humidity_label, "%s %s", str_hum, phydat_unit_to_str(humidity.unit));
+    lv_label_set_text_fmt(humidity_label, "%s #FF0000 %s#", str_hum, phydat_unit_to_str(humidity.unit));
     lv_obj_realign(humidity_label);
 }
 
@@ -150,21 +155,19 @@ static void airquality_create_temperature(lv_obj_t *parent)
     }
     LOG_INFO("Temperature sensor found\n");
 
-    lv_coord_t grid_w_meter = lv_page_get_width_grid(parent, 3, 1);
-    lv_coord_t meter_h = lv_page_get_height_fit(parent);
-    lv_coord_t meter_size = LV_MATH_MIN(grid_w_meter, meter_h);
-
     temperature_gauge = lv_gauge_create(parent, NULL);
-    lv_gauge_set_scale(temperature_gauge, 240, 31, 0);
-    lv_gauge_set_critical_value(temperature_gauge, 70);
+    lv_gauge_set_scale(temperature_gauge, 240, 21, 0);
     lv_gauge_set_range(temperature_gauge, 0, 100);
+    lv_gauge_set_critical_value(temperature_gauge, 70);
     lv_gauge_set_value(temperature_gauge, 0, 0);
-    lv_obj_set_size(temperature_gauge, meter_size, meter_size);
+    lv_obj_set_size(temperature_gauge, 80, 80);
     lv_obj_set_style_local_value_str(temperature_gauge, LV_GAUGE_PART_MAIN,
                                      LV_STATE_DEFAULT, "Temperature");
+    lv_obj_add_style(temperature_gauge, LV_GAUGE_PART_MAIN, &style_box);
 
     temperature_label = lv_label_create(temperature_gauge, NULL);
     lv_label_set_text(temperature_label, "Temperature");
+    lv_label_set_recolor(temperature_label, true);
     lv_obj_align(temperature_label, temperature_gauge, LV_ALIGN_IN_BOTTOM_MID, 0, -10);
 }
 
@@ -196,7 +199,7 @@ static void airquality_update_temperature(void)
     size_t len = fmt_s16_dfp(str_temp, temperature.val[0], temperature.scale);
     str_temp[len] = '\0';
 
-    lv_label_set_text_fmt(temperature_label, "%s %s", str_temp,
+    lv_label_set_text_fmt(temperature_label, "%s #FF0000 %s#", str_temp,
                           phydat_unit_to_str(temperature.unit));
     lv_obj_realign(temperature_label);
 }
@@ -210,8 +213,20 @@ static void airquality_create_pressure(lv_obj_t *parent)
     }
     LOG_INFO("Pressure sensor found\n");
 
-    pressure_label = lv_label_create(parent, NULL);
+    pressure_gauge = lv_gauge_create(parent, NULL);
+    lv_gauge_set_scale(pressure_gauge, 240, 21, 0);
+    lv_gauge_set_range(pressure_gauge, 0, 1089);
+    lv_gauge_set_critical_value(pressure_gauge, 1000);
+    lv_gauge_set_value(pressure_gauge, 0, 0);
+    lv_obj_set_size(pressure_gauge, 80, 80);
+    lv_obj_set_style_local_value_str(pressure_gauge, LV_GAUGE_PART_MAIN,
+                                     LV_STATE_DEFAULT, "Pressure");
+    lv_obj_add_style(pressure_gauge, LV_GAUGE_PART_MAIN, &style_box);
+
+    pressure_label = lv_label_create(pressure_gauge, NULL);
     lv_label_set_text(pressure_label, "Pressure");
+    lv_label_set_recolor(pressure_label, true);
+    lv_obj_align(pressure_label, pressure_gauge, LV_ALIGN_IN_BOTTOM_MID, 0, -15);
 }
 
 static void airquality_update_pressure(void)
@@ -226,11 +241,37 @@ static void airquality_update_pressure(void)
         return;
     }
 
-    char str_press[16];
-    size_t len = fmt_s16_dfp(str_press, pressure.val[0], pressure.scale);
-    str_press[len] = '\0';
+    /* 1 hPa == 1 mbar */
+    if (pressure.scale == 2 && pressure.unit == UNIT_PA) {
+        lv_gauge_set_value(pressure_gauge, 0, pressure.val[0]);
+        lv_label_set_text_fmt(pressure_label, "%d #FF0000 mbar#",
+                              pressure.val[0]);
+    }
+    else {
 
-    lv_label_set_text_fmt(pressure_label, "%s %s", str_press, phydat_unit_to_str(pressure.unit));
+        char str_press[32];
+        size_t len = fmt_s16_dfp(str_press, pressure.val[0], pressure.scale);
+        str_press[len] = '\0';
+
+        lv_label_set_text_fmt(pressure_label, "%s %s", str_press,
+                              phydat_unit_to_str(pressure.unit));
+    }
+    lv_obj_realign(pressure_label);
+
+    if (pressure.unit == UNIT_PA) {
+        int val = 0;
+        if (pressure.scale > 0) {
+            val = pressure.val[0] * _poor_man_pow10((unsigned)pressure.scale);
+        }
+        else if (pressure.scale < 0) {
+            val = pressure.val[0] / _poor_man_pow10((unsigned)-pressure.scale);
+        }
+        if (val > (int)(1089 * _poor_man_pow10(2))) {
+            val = 1089 * _poor_man_pow10(2);
+        }
+        /* convert to hPa/mbar */
+        lv_gauge_set_value(pressure_gauge, 0, val / _poor_man_pow10(2));
+    }
 }
 
 static void airquality_create_particulate(lv_obj_t *parent)
@@ -242,8 +283,21 @@ static void airquality_create_particulate(lv_obj_t *parent)
     }
     LOG_INFO("Particulate Matter sensor found\n");
 
-    particulate_label = lv_label_create(parent, NULL);
+    particulate_gauge = lv_gauge_create(parent, NULL);
+    lv_gauge_set_scale(particulate_gauge, 240, 21, 0);
+    lv_gauge_set_range(particulate_gauge, 0, 1100);
+    lv_gauge_set_critical_value(particulate_gauge, 500);
+    lv_gauge_set_value(particulate_gauge, 0, 0);
+    lv_obj_set_size(particulate_gauge, 80, 80);
+    lv_obj_set_style_local_value_str(particulate_gauge, LV_GAUGE_PART_MAIN,
+                                     LV_STATE_DEFAULT, "Particulate Matter");
+    lv_obj_add_style(particulate_gauge, LV_GAUGE_PART_MAIN, &style_box);
+
+    particulate_label = lv_label_create(particulate_gauge, NULL);
     lv_label_set_text(particulate_label, "Particulate Matter");
+    lv_label_set_recolor(particulate_label, true);
+    lv_obj_align(particulate_label, particulate_gauge, LV_ALIGN_IN_BOTTOM_MID, 0,
+                 -12);
 }
 
 static void airquality_update_particulate(void)
@@ -252,18 +306,20 @@ static void airquality_update_particulate(void)
         return;
     }
 
-    phydat_t particulate;
-    if (saul_reg_read(particulate_sensor, &particulate) < 0) {
-        LOG_ERROR("Couldn't read particulate sensor\n");
-        return;
+    if (particulate.scale == -6 && particulate.unit == UNIT_GPM3) {
+        lv_label_set_text_fmt(particulate_label, "%d #FF0000 ug/m3#", particulate.val[0]);
+
+        lv_gauge_set_value(particulate_gauge, 0, particulate.val[0]);
     }
-    printf("density=%d scale=%d\n", (int)particulate.val[0], (int)particulate.scale);
+    else {
+        char str_pm[32];
+        size_t len = fmt_s16_dfp(str_pm, particulate.val[0], particulate.scale);
+        str_pm[len] = '\0';
 
-    char str_pm[16];
-    size_t len = fmt_s16_dfp(str_pm, particulate.val[0], particulate.scale);
-    str_pm[len] = '\0';
-
-    lv_label_set_text_fmt(particulate_label, "%s %s", str_pm, phydat_unit_to_str(particulate.unit));
+        lv_label_set_text_fmt(particulate_label, "%s #FF0000 %s#", str_pm,
+                              phydat_unit_to_str(particulate.unit));
+    }
+    lv_obj_realign(particulate_label);
 }
 
 static void airquality_task(lv_task_t *param)
@@ -282,6 +338,12 @@ static void airquality_task(lv_task_t *param)
 
 void airquality_create(void)
 {
+    /* initialize box style */
+    lv_style_init(&style_box);
+    lv_style_set_value_align(&style_box, LV_STATE_DEFAULT, LV_ALIGN_OUT_TOP_LEFT);
+    lv_style_set_value_ofs_y(&style_box, LV_STATE_DEFAULT, - LV_DPX(15));
+    lv_style_set_margin_top(&style_box, LV_STATE_DEFAULT, LV_DPX(5));
+
     /* Air Quality window, responsive */
     win = lv_win_create(lv_disp_get_scr_act(NULL), NULL);
     lv_win_set_title(win, "Air Quality Monitor");
@@ -297,14 +359,40 @@ void airquality_create(void)
     refr_task = lv_task_create(airquality_task, REFR_TIME, LV_TASK_PRIO_LOW, NULL);
 }
 
+static char _stack[THREAD_STACKSIZE_SMALL];
+
+static void *_event_loop(void *args)
+{
+    (void)args;
+    
+    if (particulate_sensor == NULL) {
+        return NULL;
+    }
+
+    while (1) {
+        if (saul_reg_read(particulate_sensor, &particulate) < 0) {
+            LOG_ERROR("Couldn't read particulate sensor\n");
+        }
+
+        xtimer_usleep(200 * US_PER_MS);
+    }
+
+    return NULL;
+}
+
 int main(void)
 {
+#ifdef CPU_ESP32
+    LOG_INFO("Setting ADC line %d attenuation to 11 dB\n", gp2y10xx_params[0].aout);
+    adc_set_attenuation(gp2y10xx_params[0].aout, ADC_ATTENUATION_11_DB);
+#endif
+
     /* Configure the generic display driver interface */
     s_screen.display = (disp_dev_t *)&s_disp_dev;
     s_screen.display->driver = &ili9341_disp_dev_driver;
 
-    LOG_INFO("Turning on the LCD backlight\n");
     /* Enable backlight */
+    LOG_INFO("Turning on the LCD backlight\n");
     disp_dev_backlight_on();
 
     LOG_INFO("Initializing ILI9341 display\n");
@@ -315,8 +403,24 @@ int main(void)
     /* Initialize lvgl with the generic display and touch drivers */
     lvgl_init(&s_screen);
 
+    /* Dark theme */
+#if LV_USE_THEME_MATERIAL
+    LV_THEME_DEFAULT_INIT(lv_theme_get_color_primary(),
+                          lv_theme_get_color_secondary(),
+                          LV_THEME_MATERIAL_FLAG_LIGHT,
+                          lv_theme_get_font_small(),
+                          lv_theme_get_font_normal(),
+                          lv_theme_get_font_subtitle(),
+                          lv_theme_get_font_title());
+#endif
+
     /* Create the system monitor widget */
     airquality_create();
+
+    /* Create Particulate Matter thread */
+    thread_create(_stack, sizeof(_stack), THREAD_PRIORITY_MAIN - 1,
+                  THREAD_CREATE_STACKTEST, _event_loop, NULL, "gp2y10xx");
+
 
     return 0;
 }
